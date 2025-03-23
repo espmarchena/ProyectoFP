c5e33f19e449e4d808f3247004a628d0719c4458
espmarchena <espemarchena26@gmail.com>
2025-03-23 20:26:36 +0100

    actualizacion 21/03

---
+++ EDT3_CajaNegra/src/es/cesur/cajaBlanca_Ejercicio1/TestCajaNegra.java
@@ -0,0 +1,44 @@
+package es.cesur.cajaBlanca_Ejercicio1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCajaNegra {
+
+	@Test
+	void claseDeEquivalencia1() {
+		int resultadoReal = CajaNegra.diasDeMes(1);
+		int resultadoEsperado = 31;
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	@Test
+	void claseDeEquivalencia2() {
+		int resultadoReal = CajaNegra.diasDeMes(0);
+		int resultadoEsperado = 0;
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	@Test
+	void claseDeEquivalencia3() {
+		int resultadoReal = CajaNegra.diasDeMes(13);
+		int resultadoEsperado = 0;
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+
+	@Test
+	void claseDeEquivalencia4() {
+		int resultadoReal = CajaNegra.diasDeMes(4);
+		int resultadoEsperado = 30;
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	@Test
+	void claseDeEquivalencia5() {
+		int resultadoReal = CajaNegra.diasDeMes(2);
+		int resultadoEsperado = 28;
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+}
---
+++ EDT3_CajaNegra/src/es/cesur/cajaBlanca_Ejercicio4/TestCajaNegra.java
@@ -0,0 +1,14 @@
+package es.cesur.cajaBlanca_Ejercicio4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCajaNegra {
+
+	@Test
+	void testRetirar1() {
+		
+	}
+
+}
